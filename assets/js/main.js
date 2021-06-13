980a6252dbfd23f16c192187eada0125ff1f4bd8
Bogdan M <bogdanmc96@gmail.com>
2021-06-13 21:57:10 +0300

    Added dark mode.

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,24 @@ menu.addEventListener('click', function(){
   show(nav);
   });
 
+/*==================== DARK MODE BUTTON ====================*/
+
+const options = {
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
 /*==================== ACCORDION SKILLS ====================*/
 
 
